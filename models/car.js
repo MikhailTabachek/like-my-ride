09f649388b5320c6871655429a34eb1e69cab8dd
Mikhail <mishanyaveselyi@gmail.com>
2022-03-05 17:38:21 -0500

    Create Schema

---
+++ models/car.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const ratingSchema = new Schema({
+  comment: String,
+  rating: String
+}, {
+  timestamps: true
+})
+
+const carSchema = new Schema({
+  brand:{
+    type: String,
+    require: true
+  },
+  model:{
+    type: String,
+    require: true
+  },
+  year:{
+    type: Number,
+    min: 1900,
+    max: Date.now().getFullYear(),
+    require: true
+  }, 
+  color:{
+    type: String,
+    require: true
+  },
+  forSale: Boolean,
+  like: Number,
+  rating:[ratingSchema]
+}, {
+  timestamps: true
+})
+
+const Car = mongoose.model("Car", carSchema)
+
+export {
+  Car
+}
