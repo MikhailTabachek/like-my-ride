f680d560c5bb93d878f099c8ead8d1d6819e8a52
Mikhail <mishanyaveselyi@gmail.com>
2022-03-06 15:45:18 -0500

    Create new.css

--- routes/cars.js
+++ routes/cars.js
@@ -3,8 +3,9 @@ import { Router } from 'express'
 const router = Router()
 
 import * as carsCtrl from '../controllers/cars.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
-router.get('/cars', carsCtrl.index)
+router.get('/cars/new', isLoggedIn, carsCtrl.index)
 
 // router.get('/', function (req, res) {
 //   res.render('index', { title: 'Like My Ride!', user: req.user ? req.user : null })
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
 const router = Router()
+import { isLoggedIn } from '../middleware/middleware.js'
 
 import * as indexCtrl from '../controllers/index.js'
 
