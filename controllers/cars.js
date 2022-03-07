d366e606dc1ba339db70b469c85dd81bab84f913
Mikhail <mishanyaveselyi@gmail.com>
2022-03-07 18:57:22 -0500

    Remove comments from controller function

--- controllers/cars.js
+++ controllers/cars.js
@@ -2,18 +2,6 @@ import { Car } from '../models/car.js'
 import { User } from '../models/user.js'
 
 
-// function index(req, res) {
-//   console.log('Index Function!!!!!!')
-//   Car.find({}, function(error, cars){
-//   res.render('cars/index', {
-//     title: 'Like My Ride!', 
-//     user: req.user ? req.user : null, 
-//     error,
-//     cars
-//     })
-//   })
-// }
-
 function newCar(req,res){
   console.log("newwww!!!!!!")
   res.render('cars/new', {
@@ -33,12 +21,6 @@ function create(req,res){
     console.log(err)
     res.redirect('/cars/new')
   })
-  // const car = new Car(req.body)
-  // car.save(function(err){
-  //   if(err) return res.redirect('/cars/new')
-  //   res.redirect('/')
-  // })
-  // console.log(car)
 }
 
 function show(req, res){
@@ -49,7 +31,6 @@ function show(req, res){
       title: "Car Details",
       car
     })
-  // }), function (err, car){
   })
 }
 
@@ -60,7 +41,6 @@ function deleteCar(req, res){
 }
 
 export {
-  // index,
   newCar as new,
   create,
   show,
